257eee04c0091c40b9e41ccdc3ba3b231e71ab09
Eduard Mekler <emekler0729@gmail.com>
2020-03-14 22:31:32 -0500

    Smoothed transition when hands go over 0 degrees

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.minute-hand');
 const hourHand = document.querySelector('.hour-hand');
 
+const smoothTransition = function() {
+    const secondTransition = secondHand.style.transition;
+    const minuteTransition = minuteHand.style.transition;
+    const hourTransition = hourHand.style.transition;
+
+    return function(h, m, s) {
+        if (s === 59) {
+            secondHand.style.transition = 'none';
+        } else if (s === 1) {
+            secondHand.style.transition = secondTransition;
+        }
+
+        if (m === 59) {
+            minuteHand.style.transition = 'none';
+        } else if (m === 1) {
+            minuteHand.style.transition = minuteTransition;
+        }
+
+        if (h === 11) {
+            hourHand.style.transition = 'none';
+        } else if (h === 1) {
+            hourHand.style.transition = hourTransition;
+        }
+    }
+}();
+
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
@@ -15,6 +41,8 @@ function setDate() {
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    smoothTransition(hours, minutes, seconds);
 }
     
 setInterval(setDate, 1000);
